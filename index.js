761e8c96a92b308aefda496bddad8cf34809ec97
rwofford0@gmail.com <rwofford0@gmail.com>
2018-05-11 18:01:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function placeOrder(cardNumber) {
     
     
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-    cart.splice(0, cart.length)
+    
   
   } else {return 'Sorry, we dont have a credit card on file for you.'
   }
