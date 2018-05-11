219f296dd6f1fe2e0d645feba818fc7b56d33195
rwofford0@gmail.com <rwofford0@gmail.com>
2018-05-11 18:06:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function removeItemFromCart(itemToRemove) { 
 
 
 function placeOrder(cardNumber) {
-  if (cardNumber !== nil) { 
+  if (cardNumber !== null) { 
     var x = total();
     cart.splice(0, cart.length)
     
